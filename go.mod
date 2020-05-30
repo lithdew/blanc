0da9e14747fa57a90dd2a7aec771396fea3f83b4
Kenta Iwasaki <kenta@lithdew.net>
2020-05-31 04:39:34 +0900

    layout: add constraints that w,h >= 0, and child.x + child.y == parent.x + parent.y - padding, implement FillX(parent, child, ratio), implement FillY(parent, child ratio), update casso to latest version

--- go.mod
+++ go.mod
@@ -2,12 +2,10 @@ module github.com/lithdew/blanc
 
 go 1.14
 
-replace github.com/lithdew/casso => ../casso
-
 require (
 	github.com/davecgh/go-spew v1.1.0
 	github.com/gdamore/tcell v1.3.0
-	github.com/lithdew/casso v0.0.0-20200530160555-3fba2203274a
+	github.com/lithdew/casso v0.0.0-20200530184154-1f9f4b99216e
 	github.com/mattn/go-runewidth v0.0.4
 	github.com/stretchr/testify v1.5.1
 )
--- layout_test.go
+++ layout_test.go
@@ -39,24 +39,46 @@ func (b Box) Editable() {
 }
 
 func Inside(parent, child Box, padding float64) []casso.Constraint {
+	// child.w >= 0
+	// child.h >= 0
+	// parent.w >= 0
+	// parent.h >= 0
 	// child.x >= parent.x + padding
 	// child.y >= parent.y + padding
 	// child.width <= parent.width - 2 * padding
 	// child.height <= parent.height - 2 * padding
+	// child.x + child.w == parent.x + parent.w - 2 * padding
 	return []casso.Constraint{
+		casso.NewConstraint(casso.GTE, 0, child.w.T(1)),
+		casso.NewConstraint(casso.GTE, 0, child.h.T(1)),
+		casso.NewConstraint(casso.GTE, 0, parent.w.T(1)),
+		casso.NewConstraint(casso.GTE, 0, parent.h.T(1)),
 		casso.NewConstraint(casso.GTE, -padding, child.x.T(1), parent.x.T(-1)),
 		casso.NewConstraint(casso.GTE, -padding, child.y.T(1), parent.y.T(-1)),
 		casso.NewConstraint(casso.LTE, 2*padding, child.w.T(1), parent.w.T(-1)),
 		casso.NewConstraint(casso.LTE, 2*padding, child.h.T(1), parent.h.T(-1)),
+		casso.NewConstraint(casso.LTE, padding, child.x.T(1), child.w.T(1), parent.x.T(-1), parent.w.T(-1)),
 	}
 }
 
-func Fill(parent, child Box) []casso.Constraint {
-	// child.width == parent.width
-	// child.height == parent.height
+func FillX(parent, child Box, ratio float64) []casso.Constraint {
+	// child.width == ratio * parent.width
 	return []casso.Constraint{
-		casso.NewConstraint(casso.EQ, 0, child.w.T(1), parent.w.T(-1)),
-		casso.NewConstraint(casso.EQ, 0, child.h.T(1), parent.h.T(-1)),
+		casso.NewConstraint(casso.EQ, 0, child.w.T(1), parent.w.T(-ratio)),
+	}
+}
+
+func FillY(parent, child Box, ratio float64) []casso.Constraint {
+	// child.width == ratio * parent.width
+	return []casso.Constraint{
+		casso.NewConstraint(casso.EQ, 0, child.h.T(1), parent.h.T(-ratio)),
+	}
+}
+
+func SpaceBetween(a, b Box, spacing float64) []casso.Constraint {
+	// a.x + a.width + spacing == b.x
+	return []casso.Constraint{
+		casso.NewConstraint(casso.EQ, spacing, a.x.T(1), a.w.T(1), b.x.T(-1)),
 	}
 }
 
@@ -79,22 +101,38 @@ func TestBoxInside(t *testing.T) {
 	solver := casso.NewSolver()
 
 	parent := NewBox(solver)
-	child := NewBox(solver)
+	left := NewBox(solver)
+	right := NewBox(solver)
 
 	parent.Editable()
-	//child.Editable()
 
 	require.NoError(t, parent.SetX(0))
 	require.NoError(t, parent.SetY(0))
 	require.NoError(t, parent.SetW(800))
 	require.NoError(t, parent.SetH(600))
 
-	_, err := Apply(solver, casso.Required, Inside(parent, child, 10)...)
+	_, err := Apply(solver, casso.Required, Inside(parent, left, 10)...)
 	require.NoError(t, err)
 
-	_, err = Apply(solver, casso.Medium, Fill(parent, child)...)
+	_, err = Apply(solver, casso.Required, Inside(parent, right, 10)...)
+	require.NoError(t, err)
+
+	_, err = Apply(solver, casso.Weak, FillX(parent, left, 0.5)...)
+	require.NoError(t, err)
+
+	_, err = Apply(solver, casso.Weak, FillX(parent, right, 0.5)...)
+	require.NoError(t, err)
+
+	_, err = Apply(solver, casso.Weak, FillY(parent, left, 1)...)
+	require.NoError(t, err)
+
+	_, err = Apply(solver, casso.Weak, FillY(parent, right, 1)...)
+	require.NoError(t, err)
+
+	_, err = Apply(solver, casso.Required, SpaceBetween(left, right, 10)...)
 	require.NoError(t, err)
 
 	fmt.Println(parent.X(), parent.Y(), parent.W(), parent.H())
-	fmt.Println(child.X(), child.Y(), child.W(), child.H())
+	fmt.Println(left.X(), left.Y(), left.W(), left.H())
+	fmt.Println(right.X(), right.Y(), right.W(), right.H())
 }
