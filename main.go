6756b75bee3c5f40a77121f4412a624b8514e590
Kenta Iwasaki <kenta@lithdew.net>
2020-06-01 23:09:09 +0900

    textbox: initial work on text input

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"strings"
 	"time"
+	"unicode/utf8"
 )
 
 func check(err error) {
@@ -24,7 +25,8 @@ func main() {
 
 	ch := make(chan struct{})
 
-	var buf []rune
+	in := newTextbox()
+	in.setLabel(">>> ")
 
 	go func() {
 		defer close(ch)
@@ -38,25 +40,27 @@ func main() {
 				case tcell.KeyCtrlL:
 					s.Sync()
 				case tcell.KeyDelete, tcell.KeyDEL:
-					if len(buf) > 0 {
-						buf = buf[:len(buf)-1]
-					}
+					in.pop()
 				case tcell.KeyCtrlW:
-					if len(buf) > 0 {
-						i := strings.LastIndexByte(string(buf[:len(buf)-1]), ' ')
-						if i == -1 {
-							buf = buf[:0]
-						} else {
-							buf = buf[:i+1]
-						}
-					}
+					//if len(buf) > 0 {
+					//	i := strings.LastIndexByte(string(buf[:len(buf)-1]), ' ')
+					//	if i == -1 {
+					//		buf = buf[:0]
+					//	} else {
+					//		buf = buf[:i+1]
+					//	}
+					//}
 				case tcell.KeyCtrlU:
-					if len(buf) > 0 {
-						buf = buf[:0]
-					}
+				//if len(buf) > 0 {
+				//	buf = buf[:0]
+				//}
+				case tcell.KeyLeft:
+					in.moveLeft()
+				case tcell.KeyRight:
+					in.moveRight()
 				case tcell.KeyRune:
 					if ev.Key() == tcell.KeyRune {
-						buf = append(buf, ev.Rune())
+						in.push(ev.Rune())
 					}
 				}
 			case *tcell.EventResize:
@@ -105,26 +109,109 @@ loop:
 
 		// footer
 
-		renderFooter(s, scr, &buf)
+		renderFooter(s, scr, in)
 
 		s.Show()
 	}
 }
 
-func renderFooter(s tcell.Screen, scr layout.Rect, buf *[]rune) {
+type Textbox struct {
+	label []rune
+	text  []rune
+
+	ptr int // cursor start index
+	pos int // cursor end index
+}
+
+func newTextbox() *Textbox {
+	return &Textbox{}
+}
+
+func (t *Textbox) render(s tcell.Screen, style tcell.Style, r layout.Rect) {
+	start := t.ptr // selection start index
+	if start > t.pos {
+		start = t.pos
+	}
+
+	end := t.ptr // selection end index
+	if end < t.pos {
+		end = t.pos
+	}
+
+	reversed := tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorWhite) // cursor style
+
+	j := 0
+
+	for i := 0; i < len(t.label) && r.X+j < r.X+r.W-1; i, j = i+1, j+1 {
+		s.SetContent(r.X+j, r.Y, t.label[i], nil, style)
+	}
+
+	for i := 0; i < len(t.text) && r.X+j < r.X+r.W-1; i, j = i+1, j+1 {
+		if i >= start && i <= end {
+			s.SetContent(r.X+j, r.Y, t.text[i], nil, reversed)
+		} else {
+			s.SetContent(r.X+j, r.Y, t.text[i], nil, style)
+		}
+	}
+
+	if end == len(t.text) { // render cursor
+		s.SetContent(r.X+j, r.Y, tcell.RuneBlock, nil, style)
+	}
+}
+
+func (t *Textbox) moveLeft() {
+	if t.ptr == 0 {
+		return
+	}
+	t.ptr--
+	t.pos = t.ptr
+}
+
+func (t *Textbox) moveRight() {
+	if t.ptr == len(t.text) {
+		return
+	}
+	t.ptr++
+	t.pos = t.ptr
+}
+
+func (t *Textbox) moveToEnd() {
+	t.ptr = len(t.text)
+	t.pos = t.ptr
+}
+
+func (t *Textbox) push(r rune) {
+	t.text = append(t.text[:t.ptr], append([]rune{r}, t.text[t.ptr:]...)...)
+	t.moveRight()
+}
+
+func (t *Textbox) pop() rune {
+	if t.ptr == 0 {
+		return utf8.RuneError
+	}
+
+	r := t.text[t.ptr-1]
+	t.text = append(t.text[:t.ptr-1], t.text[t.ptr:]...)
+	t.moveLeft()
+	return r
+}
+
+func (t *Textbox) setLabel(label string) {
+	t.label = []rune(label)
+}
+
+func (t *Textbox) setText(text string) {
+	t.text = []rune(text)
+}
+
+func renderFooter(s tcell.Screen, scr layout.Rect, in *Textbox) {
 	style := tcell.StyleDefault.Reverse(true)
 
 	ftr := layout.Rect{W: scr.W, H: 1}.Align(scr, layout.Bottom|layout.Left)
 	clear(s, style, ftr.X, ftr.Y, ftr.X+ftr.W-1, ftr.Y+ftr.H-1)
 
-	label := ">>>"
-	labelRect := layout.Text(label).Align(ftr, layout.Left).ShiftLeft(1)
-	puts(s, style, labelRect.X, labelRect.Y, label)
+	in.render(s, style, ftr.PadLeft(1))
 
-	text := string(*buf) + string(tcell.RuneBlock)
-	textRect := layout.Text(text).Align(labelRect, layout.Right).ShiftLeft(len(text))
-	puts(s, style, textRect.X, textRect.Y, text)
-
-	menuRect := layout.Rect{X: textRect.Right() + 1, Y: textRect.Y - 10, W: 30, H: 10}
-	clear(s, tcell.StyleDefault.Background(tcell.ColorBlue), menuRect.Left(), menuRect.Top(), menuRect.Right(), menuRect.Bottom())
+	//menuRect := layout.Rect{X: textRect.Right() + 1, Y: textRect.Y - 10, W: 30, H: 10}
+	//clear(s, tcell.StyleDefault.Background(tcell.ColorBlue), menuRect.Left(), menuRect.Top(), menuRect.Right(), menuRect.Bottom())
 }
