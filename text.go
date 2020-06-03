df9fa2cd372b66417b66f44a81f1d0294eeb112f
Kenta Iwasaki <kenta@lithdew.net>
2020-06-03 18:04:09 +0900

    text, asciigraph: lazily render text only when text has changed

--- text.go
+++ text.go
@@ -10,37 +10,56 @@ import (
 type StyleFunc func(int) tcell.Style
 
 type Text struct {
+	dirty bool // does this view need to be recomputed?
+	lastW int  // last rendered width
+
 	style StyleFunc
 	wrap  bool
 
 	text  string
+	buf   []rune
 	width int
 }
 
-func NewText(text string) Text { return Text{text: text} }
+func NewText(text string) Text { return Text{text: text, lastW: -1} }
+
+func (t *Text) SetText(text string) {
+	t.dirty = true
+	t.text = text
+	t.width = runewidth.StringWidth(text)
+}
+
+func (t *Text) SetWrap(wrap bool) {
+	t.dirty = t.wrap != wrap
+	t.wrap = wrap
+}
 
 func (t *Text) SetStyle(style tcell.Style)   { t.style = func(int) tcell.Style { return style } }
 func (t *Text) SetStyleFunc(style StyleFunc) { t.style = style }
 
-func (t *Text) SetText(text string) { t.text = text; t.width = runewidth.StringWidth(text) }
-func (t *Text) SetWrap(wrap bool)   { t.wrap = wrap }
-
 func (t Text) Width() int   { return t.width }
 func (t Text) Text() string { return t.text }
 
-func (t Text) Draw(s tcell.Screen, r layout.Rect) {
+func (t *Text) Draw(s tcell.Screen, r layout.Rect) {
+	if r.W != t.lastW {
+		t.dirty = true
+		t.lastW = r.W
+	}
+
+	if t.dirty {
+		if t.wrap {
+			t.buf = []rune(linebreak.Wrap(t.text, r.W, r.W))
+		} else {
+			t.buf = []rune(t.text)
+		}
+		t.dirty = false
+	}
+
 	x := r.X
 	y := r.Y
 
-	var text []rune
-	if t.wrap {
-		text = []rune(linebreak.Wrap(t.text, r.W, r.W))
-	} else {
-		text = []rune(t.text)
-	}
-
-	for i := 0; i < len(text) && x <= r.X+r.W && y <= r.Y+r.H; i++ {
-		if text[i] == '\n' {
+	for i := 0; i < len(t.buf) && x <= r.X+r.W && y <= r.Y+r.H; i++ {
+		if t.buf[i] == '\n' {
 			y += 1
 			x = r.X
 		} else {
@@ -48,8 +67,8 @@ func (t Text) Draw(s tcell.Screen, r layout.Rect) {
 			if t.style != nil {
 				style = t.style(i)
 			}
-			s.SetContent(x, y, text[i], nil, style)
-			x += runewidth.RuneWidth(text[i])
+			s.SetContent(x, y, t.buf[i], nil, style)
+			x += runewidth.RuneWidth(t.buf[i])
 		}
 	}
 }
